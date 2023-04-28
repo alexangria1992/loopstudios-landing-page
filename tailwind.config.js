2cfa785ab85bda7acb6c08348ea21607a8c7e9ab
alexangria1992 <alex040892@gmail.com>
2023-04-28 14:01:38 +1200

    Creations Section With Gradient Overlays

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
         sans: ["Josefin Sans", "sans-serif"],
         alata: ["Alata"],
       },
+      letterSpacing: {
+        widest: ".3em",
+      },
     },
   },
   plugins: [],
